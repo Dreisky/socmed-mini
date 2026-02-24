cd7a863dbce0a1ae68f245cca70a77058611346d
Andrei Balabbo <andreibalabbo08@gmail.com>
2026-02-24 20:52:52 +0800

    post ui

--- resources/js/Pages/Home/Index.jsx
+++ resources/js/Pages/Home/Index.jsx
@@ -18,7 +18,7 @@ export default function Index() {
     return (
         <>
             <div className="grid grid-cols-[2fr_1fr] gap-4">
-                <div className="border-e pe-6">
+                <div className="border-e pe-6 space-y-3">
                     <Card>
                         <CardContent className="flex items-center gap-2">
                             <Avatar>
@@ -29,8 +29,48 @@ export default function Index() {
                                 />
                                 <AvatarFallback>CN</AvatarFallback>
                             </Avatar>
-                            <Input placeholder="Enter text" />
+                            <Button
+                                variant="outline"
+                                className="flex-1 text-start rounded-full"
+                            >
+                                What's on your mind, Shad?
+                            </Button>
                         </CardContent>
+                    </Card>
+
+                    {/* POSTS */}
+                    <Card>
+                        <CardHeader>
+                            <div className="flex items-center gap-2">
+                                <Avatar>
+                                    <AvatarImage
+                                        src="https://github.com/shadcn.png"
+                                        alt="@shadcn"
+                                        className="grayscale"
+                                    />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <div>
+                                    <p className="font-semibold text-sm">
+                                        Shad Cn
+                                    </p>
+                                    <p className="font-light text-xs">
+                                        January 12, 2021
+                                    </p>
+                                </div>
+                            </div>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="border rounded-sm text-center p-8">
+                                hello
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <div className="grid grid-cols-2 w-full gap-2">
+                                <Button variant="ghost">Like</Button>
+                                <Button variant="ghost">Comment</Button>
+                            </div>
+                        </CardFooter>
                     </Card>
                 </div>
                 <div>
